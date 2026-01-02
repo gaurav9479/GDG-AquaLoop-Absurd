e3882b9f8097026393b2f03ada2d1487ab6f774d
Roli Rathour <rolirathour368@gmail.com>
2026-01-02 21:48:48 +0530

    Add treatment ML model and API endpoint

---
+++ src/utils/simulationDefaults.js
@@ -0,0 +1,18 @@
+export const INDUSTRY_PROFILES = {
+  textile: {
+    label: "Textile Processing",
+    defaults: { bod: "420", cod: "760", ph: "5.6", turbidity: "180", tss: "360" }
+  },
+  food: {
+    label: "Food & Beverage",
+    defaults: { bod: "1200", cod: "2500", ph: "4.5", turbidity: "450", tss: "800" }
+  },
+  chemical: {
+    label: "Chemical Plant",
+    defaults: { bod: "300", cod: "1500", ph: "3.0", turbidity: "100", tss: "200" }
+  },
+  municipal: {
+    label: "Municipal Site",
+    defaults: { bod: "200", cod: "450", ph: "7.2", turbidity: "50", tss: "250" }
+  }
+};
